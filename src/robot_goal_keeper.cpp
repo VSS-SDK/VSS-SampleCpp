1ba3bf8977c1fab913e7d1c07aa43d46fa3e283e
johnathan <johnathanfercher22@gmail.com>
2016-09-08 17:38:49 -0300

    license

--- src/robot_goal_keeper.cpp
+++ src/robot_goal_keeper.cpp
@@ -1,3 +1,11 @@
+/*
+ * This file is part of the VSS-SampleStrategy project.
+ *
+ * This Source Code Form is subject to the terms of the GNU GENERAL PUBLIC LICENSE,
+ * v. 3.0. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
+ */
+ 
 #include "robot.h"
 
 void Robot::GK_calc_action(){
