413f299657a15efdf6ce49179ad674e592ca65d2
John Fercher <johnathanfercher22@gmail.com>
2018-10-06 14:44:42 -0300

    enviando debug

--- src/main.cpp
+++ src/main.cpp
@@ -6,95 +6,36 @@
  * file, You can obtain one at http://www.gnu.org/licenses/gpl-3.0/.
  */
 
-#include <Communications/StateReceiver.h>
-#include <Communications/CommandSender.h>
 #include <Communications/DebugSender.h>
-#include "cstdlib"
 
 using namespace vss;
 
-IStateReceiver *stateReceiver;
-ICommandSender *commandSender;
 IDebugSender *debugSender;
 
-State state;
-
-
-void send_commands();
-void send_debug();
-
 int main(int argc, char** argv){
-    srand(time(NULL));
-
-    stateReceiver = new StateReceiver();
-    commandSender = new CommandSender();
     debugSender = new DebugSender();
 
-    stateReceiver->createSocket();
-    commandSender->createSocket(TeamType::Yellow);
     debugSender->createSocket(TeamType::Yellow);
 
     while(true){
-        state = stateReceiver->receiveState(FieldTransformationType::None);
-        send_commands();
-        send_debug();
+        vss::Debug debug;
+
+        for(unsigned int i = 0 ; i < 3 ; i++){
+            debug.stepPoints.push_back(Point(85 + rand()%20, 65 + rand()%20));
+        }
+
+        for(unsigned int i = 0 ; i < 3 ; i++){
+            debug.finalPoses.push_back(Pose(85 + rand()%20, 65 + rand()%20, rand()%20));
+        }
+
+        for(unsigned int i = 0 ; i < 3 ; i++){
+            vss::Path path;
+            path.points.push_back(Point(85, 65));
+            path.points.push_back(Point(85 + rand()%20, 65 + rand()%20));
+        }
+
+        debugSender->sendDebug(debug);
     }
 
     return 0;
-}
-
-void send_commands(){
-    Command command;
-
-    for(int i = 0 ; i < 3 ; i++){
-        WheelsCommand wheelsCommand;
-
-        wheelsCommand.leftVel = 10;
-        wheelsCommand.rightVel = -10;
-
-        command.commands.push_back(wheelsCommand);
-    }
-
-    commandSender->sendCommand(command);
-}
-
-
-void send_debug(){
-    vss::Debug debug;
-
-    for(unsigned int i = 0 ; i < 3 ; i++){
-        vss::Point point;
-
-        point.x = state.teamYellow[i].x - 10 + rand()%20;
-        point.y = state.teamYellow[i].y - 10 + rand()%20;
-
-        debug.stepPoints.push_back(point);
-    }
-
-    for(unsigned int i = 0 ; i < 3 ; i++){
-        vss::Pose pose;
-
-        pose.x = state.teamYellow[i].x - 10 + rand()%20;
-        pose.y = state.teamYellow[i].y - 10 + rand()%20;
-        pose.angle = state.teamYellow[i].y - 10 + rand()%20;
-
-        debug.finalPoses.push_back(pose);
-    }
-
-    for(unsigned int i = 0 ; i < 3 ; i++){
-        vss::Path path;
-        vss::Point point_1;
-        vss::Point point_2;
-
-        point_1.x = state.teamYellow[i].x;
-        point_1.y = state.teamYellow[i].y;
-
-        point_2.x = state.ball.x - 10 + rand()%20;
-        point_2.y = state.ball.y - 10 + rand()%20;
-
-        path.points.push_back(point_1);
-        path.points.push_back(point_2);
-    }
-
-    debugSender->sendDebug(debug);
 }
